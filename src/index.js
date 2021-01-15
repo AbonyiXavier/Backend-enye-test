9adda8d8a80bddcae488b2dfd801bea83faf8ee7
AbonyiXavier <francisabonyi@gmail.com>
2021-01-15 06:13:13 +0100

    exchange rate api

--- src/index.js
+++ src/index.js
@@ -10,5 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/rates", exchangeRateRouter);
+z;
+app.get("/", (request, response) => {
+  response.status(200).json({
+    status: true,
+    message: "Welcome to backend-enye-test....",
+  });
+});
 
 module.exports = app;
